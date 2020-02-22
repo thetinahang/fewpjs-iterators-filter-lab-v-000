3ff0310b4f4117387009563763be122387798184
Tina Hang <mstinahang@gmail.com>
2020-02-22 17:00:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here
 const findMatching = (drivers, string) => {
-  drivers.filter( d => {
+  return drivers.filter( d => {
     return d.toUpperCase() == string.toUpperCase();
   })
 }
