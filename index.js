6daa5477d9617ed081f455d3dc71f18cca90a314
Tina Hang <mstinahang@gmail.com>
2020-02-22 17:04:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+const findMatching = (drivers, string) => {
+  return drivers.filter( d => {
+    return d.toUpperCase() == string.toUpperCase();
+  })
+}
+
+const fuzzyMatch = (drivers, string) => {
+  return drivers.filter( d => d.startsWith(string))
+}
+
+const matchName = (drivers, string) => {
+  return drivers.filter( d => d.name == string)
+}
